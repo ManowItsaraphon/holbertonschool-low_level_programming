417e0f10c857f5c816bfd63594a13f9e895d9de9
ManowItsaraphon <6571@holbertonstudents.com>
2023-04-07 22:18:22 -0700

    numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit of number
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
