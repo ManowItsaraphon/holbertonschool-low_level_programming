c6046c666dc3979fd4e838e364ccf02938c73e7f
ManowItsaraphon <6571@holbertonstudents.com>
2023-04-07 22:55:13 -0700

    hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print base 16 number in lowercase
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
