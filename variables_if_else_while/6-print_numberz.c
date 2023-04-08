7844224330b43f46ac09691cd7e4c8542795415a
ManowItsaraphon <6571@holbertonstudents.com>
2023-04-07 22:32:31 -0700

    numbers by putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print number by using putchar function
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+
+{
+	putchar(n);
+}
+	putchar('\n');
+	return (0);
+}
