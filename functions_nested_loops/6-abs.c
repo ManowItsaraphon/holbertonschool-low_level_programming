59cd1f26738dcc6f549604511c7d173f3d46cd8f
ManowItsaraphon <6571@holbertonstudents.com>
2023-04-11 01:15:01 -0700

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - to get the result in an absolute int
+ *
+ * @n: input number
+ *
+ * Return: absolute value
+ */
+
+int _abs(int n)
+{
+
+	if (n >= 0)
+
+		{
+			return (n);
+		}
+	else
+		{
+			return (n * -1);
+		}
+}
