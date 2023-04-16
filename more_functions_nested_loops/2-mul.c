9d69ba2e4818b8df9ea07f04819e94311ccbd508
ManowItsaraphon <6571@holbertonstudents.com>
2023-04-16 03:11:56 -0700

    mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multipication 2 input numbers
+ * @a: input number
+ * @b: input number
+ *
+ * Return: always 0
+ *
+ */
+
+int mul(int a, int b)
+
+{
+	return a * b;
+	
+}
