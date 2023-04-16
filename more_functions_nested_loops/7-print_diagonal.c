80194e97c6be218ab87d427cd6d96e3aa2222ec8
ManowItsaraphon <6571@holbertonstudents.com>
2023-04-16 06:18:02 -0700

    diagonally

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - to print \ for space
+ *
+ * @n: number of times
+ *
+ * Return: 0
+ *
+ */
+
+void print_diagonal(int n)
+
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+
+		{
+			_putchar(' ');
+		}
+
+		_putchar(92);
+			if (i < (n - 1))
+				_putchar('\n');
+		}
+
+	_putchar('\n');
+}
