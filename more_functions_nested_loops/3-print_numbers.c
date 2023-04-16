3c8cf21d2073c308b270e5dd1259dd878e3196d2
ManowItsaraphon <6571@holbertonstudents.com>
2023-04-16 03:36:20 -0700

    print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * void print_number - to print number 0-9
+ *
+ * return 0
+ *
+ */
+
+void print_numbers(void)
+
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
