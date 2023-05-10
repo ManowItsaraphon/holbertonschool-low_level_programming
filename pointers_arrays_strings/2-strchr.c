73bc3265229b69236586856b1496b8063b6d8f15
ManowItsaraphon <6571@holbertonstudents.com>
2023-05-09 19:19:18 -0700

    strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "main.h"
 
 /**
  * _strchr - locates a character in a string
@@ -10,12 +11,12 @@
 char *_strchr(char *s, char c)
 
 {
-	unsigned int i = 0;
+	int i;
 
-	for (; *(s + 1) != '\0'; i++)
-		if (*(s + 1) == c)
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
 			return (s + i);
-	if (*(s + i) == c)
-		return (s + i);
+	}
 	return ('\0');
 }
