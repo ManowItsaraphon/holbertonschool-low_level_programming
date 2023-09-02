8ef36207012867831740291ae3626b34d50d98cb
ManowItsaraphon <6571@holbertonstudents.com>
2023-09-02 03:22:42 -0700

    fix betty

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,31 +6,23 @@
  * @file_from: file_from
  * @file_to: file_to
  * @argv: arguments vector
- * @Return: no return
+ * Return: no return
  */
 
 void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-
-	}
 	if (file_to == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
 }
 
 /**
  * main - copy the content of a file_from to file_to file
  * @argc: number of arguments
  * @argv: argument
- * Return: 0
+ * Return: main stuff
  */
-
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
@@ -38,10 +30,7 @@ int main(int argc, char *argv[])
 	char buf[1024];
 
 	if (argc != 3)
-	{
 		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-		exit(97);
-	}
 
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
@@ -60,18 +49,11 @@ int main(int argc, char *argv[])
 
 	err_close = close(file_from);
 	if (err_close == -1)
-
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
 
 	err_close = close(file_to);
 	if (err_close == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
 
 	return (0);
 }
